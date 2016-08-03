ba637c2c669fbe96e07f98149a8714bc7d389f1c
Benjamin Silas Rhodes <benjamin.s.rhodes@gmail.com>
2016-08-02 20:17:24 -0700

    copy paste fail

--- config/prod.exs
+++ config/prod.exs
@@ -1,6 +1,6 @@
 use Mix.Config
 
-config :simple, Returb.Endpoint,
+config :returb, Returb.Endpoint,
   http: [port: {:system, "PORT"}, compress: true],
   url: [scheme: "https", host: "sleepy-shore-92135.herokuapp.com", port: 443],
   cache_static_manifest: "priv/static/manifest.json",
@@ -9,7 +9,7 @@ config :simple, Returb.Endpoint,
 
 config :logger, level: :info
 
-config :simple, Returb.Repo,
+config :returb, Returb.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
